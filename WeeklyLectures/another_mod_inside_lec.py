b12344391cd396c3b8322c30012eba17aefb4ca8
Tanvi Kansara <z5360257@ad.unsw.edu.au>
2023-11-08 12:54:24 +1100

    Initial commit

---
+++ WeeklyLectures/another_mod_inside_lec.py
@@ -0,0 +1,14 @@
+""" another_mod_inside_lec.py
+
+Another module inside the `lectures` package
+"""
+
+import mod_inside_lec
+
+""" another_mod_inside_lec.py 
+
+Another module inside the `lectures` package 
+"""
+
+# OLD: import mod_inside_lec
+from WeeklyLectures import mod_inside_lec
---
+++ toolkit_config.py
@@ -0,0 +1,9 @@
+""" toolkit_config.py
+
+Project configuration file
+"""
+import os
+
+
+PRJDIR = r'C:\Users\tanvi\PycharmProjects\Toolkit2'
+DATADIR = os.path.join(PRJDIR, 'data')
